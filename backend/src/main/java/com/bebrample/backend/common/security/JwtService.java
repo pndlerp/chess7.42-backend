dddd26be36b91eef4229761e90976e7079fb4ede
paerma <555rostik556@ukr.net>
2026-07-16 11:46:13 +0200

    Add jwt token authentication

---
+++ backend/src/main/java/com/bebrample/backend/common/security/JwtService.java
@@ -0,0 +1,33 @@
+package com.bebrample.backend.common.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final String secret = "govnogovnogovnogovnogovnogovnogovno";
+
+    private final long expirationMs = 86400000;
+
+    public String generateToken(String username){
+        return JWT.create()
+                .withSubject(username)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationMs))
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+    public String getUsernameAndVerify(String token){
+        return JWT.require(Algorithm.HMAC256(secret))
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+}
