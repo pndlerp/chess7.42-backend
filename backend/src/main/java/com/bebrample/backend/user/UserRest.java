55098fe5206e5756fc765480dda4725def5a6484
paerma <555rostik556@ukr.net>
2026-07-16 18:31:02 +0200

    Create login, register, rooms endpoints

---
+++ backend/src/main/java/com/bebrample/backend/user/UserRest.java
@@ -0,0 +1,29 @@
+package com.bebrample.backend.user;
+
+import com.bebrample.backend.user.dto.UserCreateDto;
+import com.bebrample.backend.user.dto.UserResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class UserRest {
+
+    private final UserService userService;
+    private final UserLoginDetailsService userLoginDetailsService;
+
+    @PostMapping("/register")
+    public UserResponseDto createAccount(@RequestBody UserCreateDto dto){
+        return userService.save(dto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> logintoAccount(@RequestBody UserCreateDto dto){
+        String token = userLoginDetailsService.login(dto);
+
+        return ResponseEntity.ok(token);
+
+    }
+}
