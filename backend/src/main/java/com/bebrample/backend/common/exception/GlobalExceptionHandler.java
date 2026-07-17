b833c2a4c87c74dd289c1255c910749a2cdf2bee
paerma <555rostik556@ukr.net>
2026-07-17 19:30:06 +0200

    add typemismatch exception

--- backend/src/main/java/com/bebrample/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/bebrample/backend/common/exception/GlobalExceptionHandler.java
@@ -2,17 +2,18 @@ package com.bebrample.backend.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(RoomAlreadyFullException.class)
-    public ResponseEntity<ErrorResponse> handleRoomAlreadyFull(RoomAlreadyFullException e){
+    @ExceptionHandler(RoomException.class)
+    public ResponseEntity<ErrorResponse> handleRoomAlreadyFull(RoomException e){
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setStatus(HttpStatus.CONFLICT.value());
-        errorResponse.setError("Room already full");
+        errorResponse.setError("Room logic type shi");
         errorResponse.setMessage(e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
@@ -23,5 +24,13 @@ public class GlobalExceptionHandler {
         errorResponse.setError("Resources not Found");
         errorResponse.setMessage(e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException e){
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setError("Must be a variable of integer type");
+        errorResponse.setMessage(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
