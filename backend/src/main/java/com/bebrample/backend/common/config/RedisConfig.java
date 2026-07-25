411c185a08631dc783dd64d8c1d60d0c069a6378
paerma <555rostik556@ukr.net>
2026-07-25 16:09:26 +0200

    fix RedisConfig and RoomService to use redistemplate instead of cache manager

--- backend/src/main/java/com/bebrample/backend/common/config/RedisConfig.java
+++ backend/src/main/java/com/bebrample/backend/common/config/RedisConfig.java
@@ -1,19 +1,74 @@
 package com.bebrample.backend.common.config;
 
+import com.bebrample.backend.match.Match;
 import com.bebrample.backend.room.Room;
+import com.bebrample.backend.room.ws.dto.MoveDto;
+import com.bebrample.backend.user.entity.LobbyParticipant;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Match> redisMatchTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper){
+        RedisTemplate<String, Match> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(redisConnectionFactory);
+
+        RedisSerializer<String> serializer = RedisSerializer.string();
+        JacksonJsonRedisSerializer<Match> jsonSerializer = new JacksonJsonRedisSerializer<>(objectMapper, Match.class);
+        template.setKeySerializer(serializer);
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(serializer);
+        template.setHashValueSerializer(jsonSerializer);
+        return template;
+    }
+    @Bean
+    public RedisTemplate<String, Room> redisRoomTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper){
+        RedisTemplate<String, Room> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        RedisSerializer<String> serializer = new StringRedisSerializer();
+        JacksonJsonRedisSerializer<Room> jsonSerializer = new JacksonJsonRedisSerializer<>(objectMapper,Room.class);
+        template.setKeySerializer(serializer);
+        template.setValueSerializer(jsonSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+    @Bean
+    public RedisTemplate<String, String> redisUserTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper){
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        RedisSerializer<String> serializer = new StringRedisSerializer();
+        JacksonJsonRedisSerializer<LobbyParticipant> jsonSerializer = new JacksonJsonRedisSerializer<>(objectMapper,LobbyParticipant.class);
+        template.setKeySerializer(serializer);
+        template.setValueSerializer(serializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+    @Bean
+    public RedisTemplate<String, MoveDto> redisMoveTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        RedisTemplate<String, MoveDto> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        RedisSerializer<String> serializer = new StringRedisSerializer();
+        JacksonJsonRedisSerializer<MoveDto> jsonSerializer = new JacksonJsonRedisSerializer<>(objectMapper, MoveDto.class);
+        template.setKeySerializer(serializer);
+        template.setValueSerializer(jsonSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory){
            RedisCacheConfiguration roomCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
@@ -30,8 +85,15 @@ public class RedisConfig {
                 // Значення — ТЕЖ чисті рядки String (ідеально для простого ID кімнати)
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()));
 
+        RedisCacheConfiguration historyCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(15)) // наприклад, сесія живе 15 хв
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()));
+
            return RedisCacheManager.builder(redisConnectionFactory)
                    .cacheDefaults(roomCacheConfiguration)
+                   .withCacheConfiguration("ROOM_CACHE", roomCacheConfiguration)
                    .withCacheConfiguration("PLAYER_SESSION_CACHE", sessionConfiguration)
                    .build();
     }
--- backend/src/main/java/com/bebrample/backend/room/RoomService.java
+++ backend/src/main/java/com/bebrample/backend/room/RoomService.java
@@ -2,65 +2,132 @@ package com.bebrample.backend.room;
 
 import com.bebrample.backend.common.exception.ResourcesNotFoundException;
 import com.bebrample.backend.common.exception.RoomException;
-import com.bebrample.backend.user.entity.*;
+import com.bebrample.backend.match.Match;
+import com.bebrample.backend.match.MatchRepository;
+import com.bebrample.backend.room.ws.dto.MoveDto;
 import com.bebrample.backend.user.UserRepository;
-import jakarta.persistence.Lob;
-import jakarta.transaction.Transactional;
+import com.bebrample.backend.user.entity.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-    // TODO: match history. make 1 match cache and add values to FEN field.
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RoomService {
-    private final UserRepository userRepository;
     private final LobbyParticipantMapper lobbyParticipantMapper;
-    private final CacheManager cacheManager;
+    private final RedisTemplate<String,Room> redisRoomTemplate;
+    private final RedisTemplate<String,String> stringRedisTemplate;
+    private final RedisTemplate<String, MoveDto> redisMoveDtoTemplate;
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private final MatchRepository matchRepository;
+    private final UserRepository userRepository;
 
-    public Room createRoom(){
+    private static final String CACHE_ROOM_KEY_PREFIX = "room:state:";
+    private static final String CACHE_MOVES_KEY_PREFIX = "room:moves:";
+    private static final String CACHE_LOBBY_PARTICIPANT_KEY_PREFIX = "user:";
 
-        LobbyParticipant user = getUserFromContext();
+        public Room createRoom(){
 
-        Cache playerCache = cacheManager.getCache("PLAYER_SESSION_CACHE");
-        String player = playerCache.get(user.getId(), String.class);
-        if(player != null) throw new RoomException("You already playing!");
+            LobbyParticipant user = getUserFromContext();
 
-        UserLobbyDto dto = lobbyParticipantMapper.toUserLobbyDto(user);
-        Room room = new Room();
-        room.setFirstPlayer(dto);
-        room.setSecondPlayer(null);
-        room.setRoomState(null);
+            String player = stringRedisTemplate.opsForValue()
+                    .get(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + user.getId());
+            if(player != null) throw new RoomException("You already playing!");
 
-        playerCache.put(user.getId(), room.getUuid());
-        Cache roomCache = cacheManager.getCache("ROOM_CACHE");
-        roomCache.put(room.getUuid(), room);
-        return room;
-    }
-    public Room connectToRoom(String roomUuid){
+            UserLobbyDto dto = lobbyParticipantMapper.toUserLobbyDto(user);
+            Room room = new Room();
+            room.setFirstPlayer(dto);
+            room.setSecondPlayer(null);
+            room.setRoomState(null);
 
-        LobbyParticipant user = getUserFromContext();
-
-
-        Cache playerCache = cacheManager.getCache("PLAYER_SESSION_CACHE");
-        String player = playerCache.get(user.getId(), String.class);
-        if (player != null) throw new RoomException("You already playing!");
-
-        Cache roomCache = cacheManager.getCache("ROOM_CACHE");
-        Room room = roomCache.get(roomUuid, Room.class);
-
-
-        UserLobbyDto dto = lobbyParticipantMapper.toUserLobbyDto(user);
-
-            room.setSecondPlayer(dto);
-            roomCache.put(room.getUuid(), room);
-            playerCache.put(user.getId(), room.getUuid());
+            stringRedisTemplate.opsForValue()
+                    .set(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + user.getId(),
+                            room.getUuid(), Duration.ofMinutes(15));
+            redisRoomTemplate.opsForValue()
+                    .set(CACHE_ROOM_KEY_PREFIX + room.getUuid(),
+                            room, Duration.ofMinutes(15));
             return room;
-    }
+        }
+        public Room connectToRoom(String roomUuid){
+
+            LobbyParticipant user = getUserFromContext();
+
+
+            String player = stringRedisTemplate.opsForValue()
+                    .get(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + user.getId());
+            if (player != null) throw new RoomException("You already playing!");
+            Room room = redisRoomTemplate.opsForValue()
+                    .get(CACHE_ROOM_KEY_PREFIX + roomUuid);
+
+            UserLobbyDto dto = lobbyParticipantMapper.toUserLobbyDto(user);
+            room.setSecondPlayer(dto);
+
+            redisRoomTemplate.opsForValue()
+                    .set(CACHE_ROOM_KEY_PREFIX + room.getUuid(),
+                            room, Duration.ofMinutes(30));
+            stringRedisTemplate.opsForValue()
+                    .set(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + user.getId(),
+                            room.getUuid(), Duration.ofMinutes(30));
+            return room;
+        }
+
+        public void updateChessState(String roomUuid, MoveDto move){
+            String key = CACHE_MOVES_KEY_PREFIX + roomUuid;
+            redisMoveDtoTemplate.opsForList().rightPush(key, move);
+            redisMoveDtoTemplate.expire(key, Duration.ofMinutes(30));
+
+            if(move.getTo().equals("ee") && move.getFrom().equals("ee")) endGame(roomUuid);
+        }
+
+        private void endGame(String roomUuid){
+            String movesKey = CACHE_MOVES_KEY_PREFIX + roomUuid;
+            Room room = redisRoomTemplate.opsForValue()
+                    .get(CACHE_ROOM_KEY_PREFIX + roomUuid);
+            if(room == null){
+                log.info("room is null");
+                return;
+            }
+
+            List<MoveDto> moves = redisMoveDtoTemplate.opsForList().range(movesKey, 0, -1);
+
+            if(!(room.getSecondPlayer() != null && (room.getSecondPlayer().getId() > 0 && room.getFirstPlayer().getId() > 0))) {
+                log.info("game played with guest/not full lobby. prevent from saving");
+                return;
+            }
+            Match match = new Match();
+            match.setRoomUuid(room.getUuid());
+            User user1 = userRepository.findUserById(room.getFirstPlayer().getId())
+                    .orElseThrow(() -> new ResourcesNotFoundException("User not found in db"));
+            User user2 = userRepository.findUserById(room.getSecondPlayer().getId())
+                    .orElseThrow(() -> new ResourcesNotFoundException("User not found in db"));
+            match.setBlackPlayer(user1);
+            match.setWhitePlayer(user2);
+            match.setFinalFen(null);
+            match.setResult(null);
+            match.setPgn(generatePgn(moves));
+
+            matchRepository.save(match);
+            log.info("saved game with room id:{}", roomUuid);
+        }
+
+        private String generatePgn(List<MoveDto> moves){
+            return moves.stream()
+                    .map(move -> move.getFrom() + "-" + move.getTo())
+                    .collect(Collectors.joining(" "));
+        }
+
+
+
+
 
         private LobbyParticipant getUserFromContext(){
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
