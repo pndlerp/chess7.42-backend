87ad35c79f5e5950cf579cda3f16ce4299da34d7
paerma <555rostik556@ukr.net>
2026-07-26 17:21:00 +0200

    add websocket make move event, on connect event

--- backend/src/main/java/com/bebrample/backend/room/ws/RoomWebSocketController.java
+++ backend/src/main/java/com/bebrample/backend/room/ws/RoomWebSocketController.java
@@ -3,14 +3,24 @@ package com.bebrample.backend.room.ws;
 import com.bebrample.backend.room.RoomService;
 import com.bebrample.backend.room.ws.dto.MoveDto;
 import com.bebrample.backend.room.ws.dto.WebSocketEvent;
+import com.bebrample.backend.user.entity.LobbyParticipant;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class RoomWebSocketController {
@@ -18,10 +28,14 @@ public class RoomWebSocketController {
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final RoomService roomService;
     @MessageMapping("/rooms/{uuid}/move")
-    public void makeMove(@DestinationVariable String uuid, MoveDto move) {
-        simpMessagingTemplate.convertAndSend("/topic/rooms/" + uuid, new WebSocketEvent<>("MOVE",move));
+    public void makeMove(@DestinationVariable String uuid, MoveDto move, Principal principal) {
 
-        roomService.updateChessState(uuid, move);
+        if(principal == null) throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "are you even authorized?");
+
+        Authentication auth = (Authentication) principal;
+        LobbyParticipant user = (LobbyParticipant) auth.getPrincipal();
+        roomService.updateChessState(uuid, move, user);
+        log.info("uuid: {}", uuid);
     }
 
     public void sendConnectionMessage(@DestinationVariable String uuid){
---
+++ backend/src/main/java/com/bebrample/backend/room/ws/RoomWebSocketListener.java
@@ -0,0 +1,68 @@
+package com.bebrample.backend.room.ws;
+
+import com.bebrample.backend.entity.Role;
+import com.bebrample.backend.room.Room;
+import com.bebrample.backend.room.RoomService;
+import com.bebrample.backend.room.ws.dto.ConnectDto;
+import com.bebrample.backend.room.ws.dto.WebSocketEvent;
+import com.bebrample.backend.user.entity.LobbyParticipant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+import java.security.Principal;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class RoomWebSocketListener {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final RedisTemplate<String, Room> redisTemplate;
+    private final RedisTemplate<String, String> redisUserTemplate;
+    private final RoomService roomService;
+    private static final String destinationPrefix = "/room/topic/";
+    @EventListener
+    public void onConnect(SessionSubscribeEvent event){
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String destination = accessor.getDestination();
+//        roomService.createRoom();
+//        String roomUuid = destination.substring(destinationPrefix.length());
+        Principal principal = accessor.getUser();
+        UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) principal;
+        LobbyParticipant participant = (LobbyParticipant) auth.getPrincipal();
+        String roomUuid = redisUserTemplate.opsForValue().get("user:" + participant.getId());
+        Room room = redisTemplate.opsForValue().get("room:state:"+ roomUuid);
+        if(room == null) return;
+        ConnectDto dto = new ConnectDto();
+        dto.setPlayerName(participant.getUsername());
+        if(room.getFirstPlayer().getId().equals(participant.getId()) || room.getSecondPlayer().getId().equals(participant.getId()))
+            dto.setRole(Role.PLAYER);
+        else dto.setRole(Role.SPECTATOR);
+
+        dto.setRoomState(room.getRoomState());
+        if(room.getSecondPlayer() != null) {
+            if (room.getFirstPlayer().getUsername().equals(participant.getUsername()))
+                dto.setOpponentName(room.getSecondPlayer().getUsername());
+            else dto.setOpponentName(room.getFirstPlayer().getUsername());
+        }
+
+//        log.info("{}", destination);
+//        redisTemplate.opsForValue().get(roomUuid);
+
+        simpMessagingTemplate.convertAndSend(destination, new WebSocketEvent<>("CONNECTED", dto));
+    }
+
+    @EventListener
+    public void onDisconnect(SessionDisconnectEvent event){
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+
+
+    }
+}
