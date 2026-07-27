7b727833ee50b65875a8c3004c60eb3a7edf8e6b
paerma <555rostik556@ukr.net>
2026-07-27 22:21:08 +0200

    transfer redis operation to RedisRepository

---
+++ backend/src/main/java/com/bebrample/backend/room/RedisRepository.java
@@ -0,0 +1,68 @@
+package com.bebrample.backend.room;
+
+import com.bebrample.backend.common.exception.RoomException;
+import com.bebrample.backend.room.ws.dto.MoveDto;
+import com.bebrample.backend.user.entity.LobbyParticipant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisRepository {
+    private final RedisTemplate<String,Room> redisRoomTemplate;
+    private final RedisTemplate<String,String> stringRedisTemplate;
+    private final RedisTemplate<String, MoveDto> redisMoveDtoTemplate;
+
+    private static final String CACHE_ROOM_KEY_PREFIX = "room:state:";
+    private static final String CACHE_MOVES_KEY_PREFIX = "room:moves:";
+    private static final String CACHE_LOBBY_PARTICIPANT_KEY_PREFIX = "user:";
+
+    public void createUser(String roomUuid, Long participantId){
+        stringRedisTemplate.opsForValue()
+                .set(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + participantId,
+                        roomUuid, Duration.ofMinutes(30));
+    }
+
+    public String getUserRoomUuid(Long userId){
+        return stringRedisTemplate.opsForValue()
+                .get(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + userId);
+    }
+
+    public List<MoveDto> getMoves(String roomUuid){
+        return redisMoveDtoTemplate.opsForList().range(CACHE_MOVES_KEY_PREFIX + roomUuid, 0, -1);
+    }
+
+    public boolean isPlaying(Long userId){
+        String player = stringRedisTemplate.opsForValue()
+                .get(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + userId);
+        return player != null;
+    }
+
+    public Room getRoom(String roomUuid){
+            return redisRoomTemplate.opsForValue()
+                    .get(CACHE_ROOM_KEY_PREFIX + roomUuid);
+    }
+
+    public void updateRoom(Room room){
+        redisRoomTemplate.opsForValue()
+                .set(CACHE_ROOM_KEY_PREFIX + room.getUuid(),
+                        room, Duration.ofMinutes(30));
+    }
+
+    public boolean isRoomNull(String roomUuid){
+        Room room = redisRoomTemplate.opsForValue()
+                .get(CACHE_ROOM_KEY_PREFIX + roomUuid);
+        return room == null;
+    }
+
+
+
+    public void addMoveList(String roomUuid, MoveDto move){
+        redisMoveDtoTemplate.opsForList().rightPush(CACHE_MOVES_KEY_PREFIX + roomUuid, move);
+        redisMoveDtoTemplate.expire(CACHE_MOVES_KEY_PREFIX + roomUuid, Duration.ofMinutes(30));
+    }
+}
--- backend/src/main/java/com/bebrample/backend/room/RoomService.java
+++ backend/src/main/java/com/bebrample/backend/room/RoomService.java
@@ -4,50 +4,37 @@ import com.bebrample.backend.common.exception.ResourcesNotFoundException;
 import com.bebrample.backend.common.exception.RoomException;
 import com.bebrample.backend.entity.Color;
 import com.bebrample.backend.entity.RoomState;
-import com.bebrample.backend.match.Match;
-import com.bebrample.backend.match.MatchRepository;
+import com.bebrample.backend.match.SaveMatchEvent;
+import com.bebrample.backend.room.ws.dto.GameOverDto;
 import com.bebrample.backend.room.ws.dto.MoveDto;
 import com.bebrample.backend.room.ws.dto.WebSocketEvent;
-import com.bebrample.backend.user.UserRepository;
 import com.bebrample.backend.user.entity.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class RoomService {
     private final LobbyParticipantMapper lobbyParticipantMapper;
-    private final RedisTemplate<String,Room> redisRoomTemplate;
-    private final RedisTemplate<String,String> stringRedisTemplate;
-    private final RedisTemplate<String, MoveDto> redisMoveDtoTemplate;
+    private final RedisRepository redisRepository;
     private final SimpMessagingTemplate simpMessagingTemplate;
-    private final MatchRepository matchRepository;
-    private final UserRepository userRepository;
-    private boolean moveChanger;
-
-    private static final String CACHE_ROOM_KEY_PREFIX = "room:state:";
-    private static final String CACHE_MOVES_KEY_PREFIX = "room:moves:";
-    private static final String CACHE_LOBBY_PARTICIPANT_KEY_PREFIX = "user:";
+    private final ApplicationEventPublisher eventPublisher;
 
         public Room createRoom(){
 
             LobbyParticipant user = getUserFromContext();
 
-            String player = stringRedisTemplate.opsForValue()
-                    .get(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + user.getId());
-            if(player != null) throw new RoomException("You already playing!");
+
+            if(redisRepository.isPlaying(user.getId())) throw new RoomException("You already playing!");
 
             UserLobbyDto dto = lobbyParticipantMapper.toUserLobbyDto(user);
             Room room = new Room();
@@ -55,24 +42,21 @@ public class RoomService {
             room.setSecondPlayer(null);
             room.setRoomState(RoomState.WAITING_FOR_OPPONENT);
 
-            stringRedisTemplate.opsForValue()
-                    .set(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + user.getId(),
-                            room.getUuid(), Duration.ofMinutes(30));
-            redisRoomTemplate.opsForValue()
-                    .set(CACHE_ROOM_KEY_PREFIX + room.getUuid(),
-                            room, Duration.ofMinutes(30));
+            redisRepository.createUser(room.getUuid(), user.getId());
+            redisRepository.updateRoom(room);
+
             return room;
         }
         public Room connectToRoom(String roomUuid){
 
             LobbyParticipant user = getUserFromContext();
 
-
-            String player = stringRedisTemplate.opsForValue()
-                    .get(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + user.getId());
-            if (player != null) throw new RoomException("You already playing!");
-            Room room = redisRoomTemplate.opsForValue()
-                    .get(CACHE_ROOM_KEY_PREFIX + roomUuid);
+            if(redisRepository.isPlaying(user.getId())) {
+                log.info("user is playing");
+                throw new RoomException("User is playing");
+            }
+            Room room = redisRepository.getRoom(roomUuid);
+            if (room == null) throw new RoomException("Room is null");
 
             UserLobbyDto dto = lobbyParticipantMapper.toUserLobbyDto(user);
             room.setSecondPlayer(dto);
@@ -80,75 +64,57 @@ public class RoomService {
             if(room.getSecondPlayer() != null) startGame(room);
             log.info("{} <- second player", room.getSecondPlayer());
 
-            redisRoomTemplate.opsForValue()
-                    .set(CACHE_ROOM_KEY_PREFIX + room.getUuid(),
-                            room, Duration.ofMinutes(30));
-            stringRedisTemplate.opsForValue()
-                    .set(CACHE_LOBBY_PARTICIPANT_KEY_PREFIX + user.getId(),
-                            room.getUuid(), Duration.ofMinutes(30));
+            redisRepository.updateRoom(room);
+            redisRepository.createUser(room.getUuid(), user.getId());
             return room;
         }
 
         public void updateChessState(String roomUuid, MoveDto move, LobbyParticipant user){
             UserLobbyDto whoMadeMove = null;
-            String key = CACHE_MOVES_KEY_PREFIX + roomUuid;
-            Room room = redisRoomTemplate.opsForValue().get("room:state:" + roomUuid);
+            Room room = redisRepository.getRoom(roomUuid);
+            if(room == null) throw new ResourcesNotFoundException("Room is null");
             Color atStart = room.getActiveColor();
 
             if(room.getRoomState() == RoomState.FINISHED) throw new RoomException("Game is finished. its over.");
-            String redisRoomUuid = stringRedisTemplate.opsForValue().get("user:" + user.getId());
+            String redisRoomUuid = redisRepository.getUserRoomUuid(user.getId());
+            if(redisRoomUuid == null) throw new ResourcesNotFoundException("User not playing?");
             if(!redisRoomUuid.equals(roomUuid)) throw new RoomException("you aint even playin this game bradar");
 
             if(user.getId().equals(room.getFirstPlayer().getId())) whoMadeMove = room.getFirstPlayer();
              else if(user.getId().equals(room.getSecondPlayer().getId())) whoMadeMove = room.getSecondPlayer();
+
+             if(whoMadeMove == null) throw new ResourcesNotFoundException("how");
             if(whoMadeMove.getColor() != room.getActiveColor()) throw new RoomException("not your move");
-            redisMoveDtoTemplate.opsForList().rightPush(key, move);
-            redisMoveDtoTemplate.expire(key, Duration.ofMinutes(30));
+            redisRepository.addMoveList(roomUuid, move);
             simpMessagingTemplate.convertAndSend("/topic/rooms/" + roomUuid, new WebSocketEvent<>("MOVE", move));
             room.setActiveColor(atStart.toggle());
-            redisRoomTemplate.opsForValue().set("room:state:" + roomUuid, room);
+            redisRepository.updateRoom(room);
             if (move.getTo().equals("ee") && move.getFrom().equals("ee")) endGame(roomUuid);
 
         }
 
         private void endGame(String roomUuid){
-            String movesKey = CACHE_MOVES_KEY_PREFIX + roomUuid;
-            Room room = redisRoomTemplate.opsForValue()
-                    .get(CACHE_ROOM_KEY_PREFIX + roomUuid);
+
+            Room room = redisRepository.getRoom(roomUuid);
             if(room == null){
                 log.info("room is null");
                 return;
             }
             room.setRoomState(RoomState.FINISHED);
-            redisRoomTemplate.opsForValue().set("room:state:" + room.getUuid(), room);
-            List<MoveDto> moves = redisMoveDtoTemplate.opsForList().range(movesKey, 0, -1);
-
+            redisRepository.updateRoom(room);
             if(!(room.getSecondPlayer() != null && (room.getSecondPlayer().getId() > 0 && room.getFirstPlayer().getId() > 0))) {
                 log.info("game played with guest/not full lobby. prevent from saving");
                 return;
             }
-            Match match = new Match();
-            match.setRoomUuid(room.getUuid());
-            User user1 = userRepository.findUserById(room.getFirstPlayer().getId())
-                    .orElseThrow(() -> new ResourcesNotFoundException("User not found in db"));
-            User user2 = userRepository.findUserById(room.getSecondPlayer().getId())
-                    .orElseThrow(() -> new ResourcesNotFoundException("User not found in db"));
-            match.setBlackPlayer(user1);
-            match.setWhitePlayer(user2);
-            match.setFinalFen(null);
-            match.setResult(null);
-            match.setPgn(generatePgn(moves));
-
-            matchRepository.save(match);
+            List<MoveDto> moves = redisRepository.getMoves(room.getUuid());
+            SaveMatchEvent event = new SaveMatchEvent(room, moves);
+            eventPublisher.publishEvent(event);
+            GameOverDto gameOverDto = new GameOverDto("sybau", "me", "im cool");
+            simpMessagingTemplate.convertAndSend("/topic/rooms/" + roomUuid, new WebSocketEvent<>("GAME_OVER", gameOverDto));
             log.info("saved game with room id:{}", roomUuid);
-
         }
 
-        private String generatePgn(List<MoveDto> moves){
-            return moves.stream()
-                    .map(move -> move.getFrom() + "-" + move.getTo())
-                    .collect(Collectors.joining(" "));
-        }
+
 
         private void randomizeSides(Room room){
             boolean RNGGOD = ThreadLocalRandom.current().nextBoolean();
