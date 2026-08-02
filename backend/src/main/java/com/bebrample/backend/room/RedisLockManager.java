fff231d1ba91095d5d54757b84c4c1aed6484788
paerma <555rostik556@ukr.net>
2026-08-02 22:38:35 +0200

    add redis lock to prevent race conditions

---
+++ backend/src/main/java/com/bebrample/backend/room/RedisLockManager.java
@@ -0,0 +1,50 @@
+package com.bebrample.backend.room;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.ReturnType;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RedisLockManager {
+    private final StringRedisTemplate stringRedisTemplate;
+    private static final String RELEASE_LOCK_SCRIPT = """
+            if redis.call('GET', KEYS[1]) == ARGV[1] then
+            return redis.call('DEL', KEYS[1])
+                    else return 0 end
+                    """;
+
+    public String tryLock(String key, Duration ttl){
+        String lockKey = "lock:" + key;
+        String lockId = UUID.randomUUID().toString();
+
+        Boolean isLocked = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, lockId, ttl);
+        if(Boolean.TRUE.equals(isLocked)){
+            return lockId;
+        }
+        return null;
+    }
+
+    public void unlockLock(String key, String lockId){
+        String lockKey = "lock:" + key;
+
+        Long result = stringRedisTemplate.execute(connection -> connection.scriptingCommands().eval(
+                RELEASE_LOCK_SCRIPT.getBytes(StandardCharsets.UTF_8),
+                ReturnType.INTEGER,
+                1,
+                lockKey.getBytes(StandardCharsets.UTF_8),
+                lockId.getBytes(StandardCharsets.UTF_8)),
+                true);
+
+
+
+    }
+}
--- backend/src/main/java/com/bebrample/backend/room/ws/RoomWebSocketController.java
+++ backend/src/main/java/com/bebrample/backend/room/ws/RoomWebSocketController.java
@@ -1,44 +1,46 @@
 package com.bebrample.backend.room.ws;
 
+import com.bebrample.backend.common.exception.RoomException;
+import com.bebrample.backend.room.RedisLockManager;
 import com.bebrample.backend.room.RoomService;
 import com.bebrample.backend.room.ws.dto.MoveDto;
-import com.bebrample.backend.room.ws.dto.WebSocketEvent;
 import com.bebrample.backend.user.entity.LobbyParticipant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
+import java.time.Duration;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class RoomWebSocketController {
-
-    private final SimpMessagingTemplate simpMessagingTemplate;
     private final RoomService roomService;
+    private final RedisLockManager redisLockManager;
     @MessageMapping("/rooms/{uuid}/move")
     public void makeMove(@DestinationVariable String uuid, MoveDto move, Principal principal) {
+
+        String lockKey = "room:state:" + uuid;
+
+        String lockId = redisLockManager.tryLock(lockKey, Duration.ofSeconds(5));
+        if(lockId == null) throw new RoomException("You making moves very fast! wait a little");
 
         if(principal == null) throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "are you even authorized?");
 
         Authentication auth = (Authentication) principal;
         LobbyParticipant user = (LobbyParticipant) auth.getPrincipal();
-        roomService.updateChessState(uuid, move, user);
-        log.info("uuid: {}", uuid);
-    }
-
-    public void sendConnectionMessage(@DestinationVariable String uuid){
-        simpMessagingTemplate.convertAndSend("/topic/rooms/" + uuid, "user connected!");
+        try {
+            roomService.updateChessState(uuid, move, user);
+            log.info("move was made");
+        } finally {
+            redisLockManager.unlockLock(lockKey, lockId);
+        }
     }
 }
