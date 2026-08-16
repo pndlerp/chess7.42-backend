0c7a1b322b0fcd12c6631651f4d46afcfe928b2b
paerma <555rostik556@ukr.net>
2026-08-16 02:48:36 +0200

    add /me endpoint for getting loggined user

--- backend/src/main/java/com/bebrample/backend/user/UserRest.java
+++ backend/src/main/java/com/bebrample/backend/user/UserRest.java
@@ -1,14 +1,16 @@
 package com.bebrample.backend.user;
 
 import com.bebrample.backend.user.dto.UserResponseDto;
-import com.bebrample.backend.user.entity.User;
+import com.bebrample.backend.user.entity.LobbyParticipant;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -28,5 +30,13 @@ public class UserRest {
     @Operation(summary = "Get all users")
     public List<UserResponseDto> getAllUsers(){
         return userService.findAll();
+    }
+
+    @GetMapping("/me")
+    @Operation(summary = "returns info about currently logged in user")
+    public UserResponseDto getMe(Principal principal){
+        Authentication auth = (Authentication) principal;
+        LobbyParticipant user = (LobbyParticipant) auth.getPrincipal();
+        return userService.findById(user.getId());
     }
 }
